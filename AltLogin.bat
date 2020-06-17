dc11163dcee99293c9a465541060d352d27df747
GenericPNP <30364419+GenericPNP@users.noreply.github.com>
2020-06-17 18:25:23 -0400

    Delete AltLogin.bat

--- AltLogin.bat
+++
@@ -1,8 +0,0 @@
-
-
-START /MIN MinecraftClient.Exe   email password  play.cosmicpvp.com
-timeout 20
-START /MIN MinecraftClient.Exe   bgrsisdks@hotmail.com 2sl3k4ksla21!2slae   play.cosmicpvp.com
-timeout 20
-
-
